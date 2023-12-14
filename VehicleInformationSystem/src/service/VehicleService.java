d18a7dd1443312359305bd605848e8dd74626227
Ankit Pal <anpal@altimetrik.com>
2023-12-14 12:28:53 +0530

    updated branch

---
+++ VehicleInformationSystem/src/service/VehicleService.java
@@ -0,0 +1,22 @@
+package service;
+
+import dao.VehicleDao;
+import model.Vehicle;
+
+public class VehicleService{
+
+VehicleDao vehicleDao= new VehicleDao();
+    public int addVehicle(Vehicle vehicle) {
+       return  vehicleDao.addVehicleDao(vehicle);
+    }
+
+
+    public int editVehicleDetails(int vehicleNumber, Vehicle vehicle) {
+       return vehicleDao.editVehicleDetailsDao(vehicleNumber,vehicle);
+    }
+
+
+    public Vehicle displayVehicleDetails(int vehicleNumber) {
+        return vehicleDao.displayVehicleDetailsDao(vehicleNumber);
+    }
+}
